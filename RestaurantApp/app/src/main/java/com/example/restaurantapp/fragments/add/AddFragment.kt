4cda9be6afb75f60eebfe07d4967282259b84eab
Tamas <szalma_tamas@yahoo.com>
2020-12-06 20:49:02 +0200

    Fragments

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/fragments/add/AddFragment.kt
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/fragments/add/AddFragment.kt
@@ -1,22 +1,42 @@
-package com.example.restaurantapp
+package com.example.restaurantapp.fragments.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.restaurantapp.R
+import com.example.restaurantapp.data.Restaurant
+import com.example.restaurantapp.data.RestaurantViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 
 class AddFragment : Fragment() {
+
+    private lateinit var mRestaurantViewModel: RestaurantViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+        mRestaurantViewModel = ViewModelProvider(this).get(RestaurantViewModel::class.java)
+
+        view.add_btn.setOnClickListener{
+            insertDataToDatabase()
+        }
+
+        return view
     }
 
+    private fun insertDataToDatabase(){
+        val 
+        val price =
 
+    }
 }
